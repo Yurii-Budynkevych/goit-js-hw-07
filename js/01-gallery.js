b6cc7908bed03032d4e19d6c365fedbc216225e5
Yurii-Budynkevych <yurii.budynkevych@gmail.com>
2022-08-05 18:34:56 +0300

    frameworks basics add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const elements = galleryItems
+  .map((element) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+galleryEl.insertAdjacentHTML("afterbegin", elements);
+galleryEl.addEventListener("click", onClick);
+function onClick(eve) {
+  eve.preventDefault();
+  if (eve.target.nodeName !== "IMG") {
+    return;
+  }
+  const src = eve.target.dataset.source;
+  const instance = basicLightbox.create(`
+<img src="${src}" width = "800" height = "800"/>
+`);
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const elements = galleryItems
+  .map((element) => {
+    return `<li><a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a></li>`;
+  })
+  .join("");
+galleryEl.insertAdjacentHTML("afterbegin", elements);
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
